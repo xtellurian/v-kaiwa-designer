3000f80107a3d4362c4b85ad647749ceaafc7250
Rian Finnegan <flanagan89@gmail.com>
2018-05-18 09:13:46 +1000

    Implemented getting info from table store for Intents

--- V-KAIWA-Designer/Repository/TableStoreIntentRepository.cs
+++ V-KAIWA-Designer/Repository/TableStoreIntentRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,20 +9,39 @@ namespace V_KAIWA_Designer.Repository
 {
     public class TableStoreIntentRepository : IIntentRepository
     {
+        public bool IsAvailable { get; private set; }
+
         private string _connectionString;
+        private string _tableName;
+        private CloudStorageAccount _storageAccount;
 
         public TableStoreIntentRepository()
         {
-            _connectionString = Program.Configuration["TableStorageConnectionString"];
+            // these come from App Settings
+            _connectionString = Program.Configuration["Storage:ConnectionString"];
+            _tableName = Program.Configuration["Storage:TableName"];
+
             IsAvailable = ! string.IsNullOrEmpty(_connectionString);
-
+            if(IsAvailable) _storageAccount = CloudStorageAccount.Parse(_connectionString);
         }
 
-        public bool IsAvailable { get; private set; }
 
-        Task<IEnumerable<string>> IIntentRepository.GetIntents()
+        async Task<IEnumerable<string>> IIntentRepository.GetIntents()
         {
-            throw new NotImplementedException();
+            // get from table storage
+            var tableClient = _storageAccount.CreateCloudTableClient();
+            var table = tableClient.GetTableReference(_tableName);
+            var query = new TableQuery<IntentTableEntity>();
+            TableContinuationToken token = null; // if there's more than 1000 rows in the table, this will need to be checked
+            var queryResult = await table.ExecuteQuerySegmentedAsync(query, token);
+            var result = queryResult.ToList();
+
+            return result.Select(s => s.Name);
         }
+    }
+
+    public class IntentTableEntity : TableEntity
+    {
+        public string Name { get; set; }
     }
 }
